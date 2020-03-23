167d955dc7168732228e4f992ccee5c5ab341f49
SW10 <SW10@172.18.57.112>
2020-03-23 16:27:18 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Select Your Theme");
+		System.out.println("1. Restaurant");
+		System.out.println("2. Cafe");
+		System.out.println("3. Accommodation");
+		System.out.println("4. Festival");
+		System.out.println("5. Public Performance");
+		System.out.println("6. Tourist Spot");
+		System.out.println("7. See You Again");
+	}
+}
