d67a8d642868e6ce5c8df9a00703b3dd59cbe9f1
SW10 <SW10@172.18.57.112>
2020-03-30 16:41:06 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,13 +1,80 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Select Your Theme");
-		System.out.println("1. Restaurant");
-		System.out.println("2. Cafe");
-		System.out.println("3. Accommodation");
-		System.out.println("4. Festival");
-		System.out.println("5. Public Performance");
-		System.out.println("6. Tourist Spot");
-		System.out.println("7. See You Again");
+		Scanner input = new Scanner(System.in);
+		int num = 17;
+
+		while (num !=18) {
+			System.out.println("*** Place Recommendation ***");
+			System.out.println("Select Your Theme");
+			System.out.println("1. Add Restaurant, Cafe");
+			System.out.println("2. Add Accommodation");
+			System.out.println("3. Add Festival");
+			System.out.println("4. Add Tourist Spot");
+			System.out.println("5. Delete Restaurant, Cafe");
+			System.out.println("6. Delete Accommodation");
+			System.out.println("7. Delete FestivalTourist Spot");
+			System.out.println("8. Delete Tourist Spot");
+			System.out.println("9. Edit Restaurant, Cafe");
+			System.out.println("10. Edit Accommodation");
+			System.out.println("11. Edit Festival");
+			System.out.println("12. Edit Tourist Spot");
+			System.out.println("13. View Restaurant, Cafe");
+			System.out.println("14. View Accommodation");
+			System.out.println("15. View Festival");
+			System.out.println("16. View Tourist Spot");
+			System.out.println("17. Show Menu");
+			System.out.println("18. See You Again");
+			num = input.nextInt();
+			if (num == 1) {
+				addRestaurantCafe ();
+			}
+			else if (num == 5) {
+				deleteRestaurantCafe ();
+			}
+			else if (num == 9) {
+				editRestaurantCafe ();
+			}
+			else if (num == 13) {
+				viewRestaurantCafe ();
+			}
+			else {
+				continue;
+			}
+		}
+	}
+
+	public static void addRestaurantCafe () {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Area:");
+		String Area = input.next();
+		System.out.print("Name");
+		String Name = input.next();
+		System.out.print("Food:");
+		String Food = input.next();
+		System.out.print("Price:");
+		int Price = input.nextInt();
+	}
+	
+	public static void deleteRestaurantCafe () {	
+		Scanner input = new Scanner(System.in);
+		System.out.print("Area:");
+		String Area = input.next();
+	}
+	
+    public static void editRestaurantCafe () {	
+    	Scanner input = new Scanner(System.in);
+    	System.out.print("Area:");
+		String Area = input.next();
+	}
+    
+    public static void viewRestaurantCafe () {	
+    	Scanner input = new Scanner(System.in);
+    	System.out.print("Area:");
+		String Area = input.next();
 	}
 }
+	
+
